3849279caab45db89d9f877d3726b7467249f626
Kenji Saito <k.saito.1985@gmail.com>
2017-12-28 14:29:34 +0100

    update cameraController to support mobile

--- src/cameraController.js
+++ src/cameraController.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('wolfy87-eventemitter');
+const TweenLite = require('gsap/TweenLite');
 import { Vector3 } from 'tubugl-math/src/vector3';
 import { mathUtils } from 'tubugl-utils';
 import { vec3 } from 'gl-matrix';
@@ -42,11 +43,13 @@ export class CameraController extends EventEmitter {
 		this.enableKeys = true;
 
 		// The four arrow keys
-		this.keys = { LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40 };
+		this.keys = { LEFT: 37, UP: 38, RIGHT: 39, BOTTOM: 40, SHIFT: 16 };
 
 		// for reset
 		this.originTarget = new Vector3();
 		this.originPosition = new Vector3(this._camera.position.x, this._camera.position.y, this._camera.position.z);
+
+		this._isShiftDown = false;
 
 		this._rotateStart = { x: null, y: null };
 		this._rotateEnd = { x: null, y: null };
@@ -59,6 +62,10 @@ export class CameraController extends EventEmitter {
 		let theta = Math.atan2(this._camera.position.x, this._camera.position.z); // equator angle around y-up axis
 		let phi = Math.acos(mathUtils.clamp(this._camera.position.y / radius, -1, 1)); // polar angle
 		this._spherical = { radius: radius, theta: theta, phi: phi };
+
+		this._pan = {
+			axis: {}
+		};
 
 		this._bindEvens();
 		this.setEventHandler();
@@ -73,6 +80,7 @@ export class CameraController extends EventEmitter {
 		this.domElement.addEventListener('touchmove', this._touchMoveHandler, false);
 
 		window.addEventListener('keydown', this._onKeyDownHandler, false);
+		window.addEventListener('keyup', this._onKeyUpHandler, false);
 	}
 	removeEventHandler() {
 		this.domElement.removeEventListener('contextmenu', this._contextMenuHandler, false);
@@ -86,6 +94,7 @@ export class CameraController extends EventEmitter {
 		this.domElement.removeEventListener('touchmove', this._touchMoveHandler, false);
 
 		window.removeEventListener('keydown', this._onKeyDownHandler, false);
+		window.removeEventListener('keydown', this._onKeyUpHandler, false);
 	}
 	update() {
 		let s = this._spherical;
@@ -109,6 +118,7 @@ export class CameraController extends EventEmitter {
 		this._touchMoveHandler = this._touchMoveHandler.bind(this);
 
 		this._onKeyDownHandler = this._onKeyDownHandler.bind(this);
+		this._onKeyUpHandler = this._onKeyUpHandler.bind(this);
 	}
 
 	_contextMenuHandler(event) {
@@ -144,8 +154,7 @@ export class CameraController extends EventEmitter {
 				y: this._rotateEnd.y - this._rotateStart.y
 			};
 
-			this._spherical.theta += -this._roatteDelta.x / this.domElement.clientWidth * Math.PI;
-			this._spherical.phi += -this._roatteDelta.y / this.domElement.clientHeight * Math.PI;
+			this._updateRotateHandler();
 
 			this._rotateStart = { x: this._rotateEnd.x, y: this._rotateEnd.y };
 		} else if (this.state === 'pan') {
@@ -155,21 +164,7 @@ export class CameraController extends EventEmitter {
 				y: 0.5 * (this._panEnd.y - this._panStart.y)
 			};
 
-			let xDir = vec3.create();
-			let yDir = vec3.create();
-			let zDir = vec3.create();
-			zDir[0] = this.target.x - this._camera.position.x;
-			zDir[1] = this.target.y - this._camera.position.y;
-			zDir[2] = this.target.z - this._camera.position.z;
-			vec3.normalize(zDir, zDir);
-
-			vec3.cross(xDir, zDir, [0, 1, 0]);
-			vec3.cross(yDir, xDir, zDir);
-
-			this.target.x += xDir[0] * this._panDelta.x + yDir[0] * this._panDelta.y;
-			this.target.y += xDir[1] * this._panDelta.x + yDir[1] * this._panDelta.y;
-			this.target.z += xDir[2] * this._panDelta.x + yDir[2] * this._panDelta.y;
-
+			this._updatePanHandler();
 			this._panStart = { x: this._panEnd.x, y: this._panEnd.y };
 		}
 		this.update();
@@ -186,11 +181,145 @@ export class CameraController extends EventEmitter {
 		this.update();
 	}
 
-	_touchStartHandler(event) {}
+	_touchStartHandler(event) {
+		let dX, dY;
+		switch (event.touches.length) {
+			case 1:
+				this.state = 'rotate';
+				this._rotateStart = { x: event.touches[0].clientX, y: event.touches[0].clientY };
+				break;
+			case 2:
+				this.state = 'zoom';
+				dX = event.touches[1].clientX - event.touches[0].clientX;
+				dY = event.touches[1].clientY - event.touches[0].clientY;
+				this._zoomDistance = Math.sqrt(dX * dX + dY * dY);
+				break;
+			case 3:
+				this.state = 'pan';
+				this._panStart = {
+					x: (event.touches[0].clientX + event.touches[1].clientX + event.touches[2].clientX) / 3,
+					y: (event.touches[0].clientY + event.touches[1].clientY + event.touches[2].clientY) / 3
+				};
 
-	_touchMoveHandler(event) {}
+				break;
+		}
+	}
+
+	_touchMoveHandler(event) {
+		let dX, dY, dDis;
+		switch (event.touches.length) {
+			case 1:
+				if (this.state !== 'rotate') return;
+				this._rotateEnd = { x: event.touches[0].clientX, y: event.touches[0].clientY };
+				this._roatteDelta = {
+					x: (this._rotateEnd.x - this._rotateStart.x) * 0.5,
+					y: (this._rotateEnd.y - this._rotateStart.y) * 0.5
+				};
+
+				this._updateRotateHandler();
+
+				this._rotateStart = { x: this._rotateEnd.x, y: this._rotateEnd.y };
+				break;
+			case 2:
+				if (this.state !== 'zoom') return;
+				dX = event.touches[1].clientX - event.touches[0].clientX;
+				dY = event.touches[1].clientY - event.touches[0].clientY;
+				this._zoomDistanceEnd = Math.sqrt(dX * dX + dY * dY);
+
+				dDis = this._zoomDistanceEnd - this._zoomDistance;
+				dDis *= 1.5;
+
+				var targetRadius = this._spherical.radius - dDis;
+				targetRadius = mathUtils.clamp(targetRadius, this.minDistance, this.maxDistance);
+				this._zoomDistance = this._zoomDistanceEnd;
+
+				TweenLite.killTweensOf(this._spherical);
+				TweenLite.to(this._spherical, 0.3, {
+					radius: targetRadius,
+					onUpdate: () => {
+						this.update();
+					}
+					// ease: Quint.easeOut
+				});
+
+				break;
+			case 3:
+				this._panEnd = {
+					x: (event.touches[0].clientX + event.touches[1].clientX + event.touches[2].clientX) / 3,
+					y: (event.touches[0].clientY + event.touches[1].clientY + event.touches[2].clientY) / 3
+				};
+				this._panDelta = {
+					x: this._panEnd.x - this._panStart.x,
+					y: this._panEnd.y - this._panStart.y
+				};
+
+				this._updatePanHandler();
+				this._panStart = { x: this._panEnd.x, y: this._panEnd.y };
+				break;
+		}
+
+		this.update();
+	}
 
 	_touchEndHandler(event) {}
 
-	_onKeyDownHandler(event) {}
+	_onKeyDownHandler(event) {
+		let dX = 0,
+			dY = 0;
+
+		switch (event.keyCode) {
+			case this.keys.SHIFT:
+				this._isShiftDown = true;
+				break;
+			case this.keys.LEFT:
+				dX = -10;
+				break;
+			case this.keys.RIGHT:
+				dX = 10;
+				break;
+			case this.keys.UP:
+				dY = 10;
+				break;
+			case this.keys.BOTTOM:
+				dY = -10;
+				break;
+		}
+
+		if (!this._isShiftDown) {
+			this._panDelta = { x: dX, y: dY };
+			this._updatePanHandler();
+		} else {
+			this._roatteDelta = { x: -dX, y: dY };
+			this._updateRotateHandler();
+		}
+
+		this.update();
+	}
+	_onKeyUpHandler(event) {
+		switch (event.keyCode) {
+			case this.keys.SHIFT:
+				this._isShiftDown = false;
+				break;
+		}
+	}
+	_updatePanHandler() {
+		let xDir = vec3.create();
+		let yDir = vec3.create();
+		let zDir = vec3.create();
+		zDir[0] = this.target.x - this._camera.position.x;
+		zDir[1] = this.target.y - this._camera.position.y;
+		zDir[2] = this.target.z - this._camera.position.z;
+		vec3.normalize(zDir, zDir);
+
+		vec3.cross(xDir, zDir, [0, 1, 0]);
+		vec3.cross(yDir, xDir, zDir);
+
+		this.target.x += xDir[0] * this._panDelta.x + yDir[0] * this._panDelta.y;
+		this.target.y += xDir[1] * this._panDelta.x + yDir[1] * this._panDelta.y;
+		this.target.z += xDir[2] * this._panDelta.x + yDir[2] * this._panDelta.y;
+	}
+	_updateRotateHandler() {
+		this._spherical.theta += -this._roatteDelta.x / this.domElement.clientWidth * Math.PI;
+		this._spherical.phi += -this._roatteDelta.y / this.domElement.clientHeight * Math.PI;
+	}
 }
